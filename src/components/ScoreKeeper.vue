e74383eda66cb1d2de0f5defedaf37320dac1454
Ryan Guill <ryanguill@gmail.com>
2020-01-07 21:49:59 -0600

    add streak to state and to score keeper.

--- src/components/ScoreKeeper.vue
+++ src/components/ScoreKeeper.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="score-keeper-card">
     <div class="score-keeper">
-      <div class="right score">{{ right_count }}</div>
+      <div class="right">
+        <div class="score">{{ right_count }}</div>
+
+      </div>
       <div class="wrong score">{{ wrong_count }}</div>
+      <div class="current-streak">{{ streak }}</div>
       <div style="clear: both;"></div>
     </div>
   </div>
@@ -16,17 +20,21 @@ export default {
   data: function() {
     return {
       right_count: 0,
-      wrong_count: 0
+      wrong_count: 0,
+      streak: 0
     };
   },
   methods: {
     update(state) {
+      console.log("update", state); // eslint-disable-line
       this.right_count = state.right_count;
       this.wrong_count = state.wrong_count;
+      this.streak = state.streak;
     }
   },
   created: function() {
     EventBus.$on("STATE_CHANGE", this.update);
+    this.update(EventBus.getState());
   }
 };
 </script>
@@ -44,7 +52,11 @@ export default {
   font-weight: bold;
   font-size: 35pt;
   text-align: center;
-  width: 50%;
+
+}
+
+.score-keeper {
+  position: relative;
 }
 
 .right {
@@ -52,13 +64,28 @@ export default {
   background-color: #9bc53d;
   border-top-left-radius: 10px;
   border-bottom-left-radius: 10px;
+  width: 50%;
+  position:relative;
 }
 
 .wrong {
   float: right;
   background-color: #c3423f;
   color: #fff;
+  width: 50%;
   border-top-right-radius: 10px;
   border-bottom-right-radius: 10px;
 }
+
+.current-streak {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin: 0 auto;
+  width: 50px;
+  font-size: 20pt;
+  background-color: gold;
+}
+
 </style>
--- src/eventBus.js
+++ src/eventBus.js
@@ -2,7 +2,8 @@ import Vue from "vue";
 
 let state = {
   right_count: 0,
-  wrong_count: 0
+  wrong_count: 0,
+	streak: 0,
 };
 
 export const EventBus = new Vue({
@@ -13,20 +14,35 @@ export const EventBus = new Vue({
     incorrect() {
       this.$emit("INCORRECT_ANSWER");
     },
-    stateChange() {
+    loadState(newState) {
+      console.log("loadstate", newState); // eslint-disable-line
+      state = newState;
+      this.stateChange({ persistToLocalStorage: false });
+    },
+    stateChange({ persistToLocalStorage = true } = {}) {
+      console.log("stateChange", persistToLocalStorage); // eslint-disable-line
       this.$emit("STATE_CHANGE", this.getState());
+      if (persistToLocalStorage) {
+        console.log("persisting to local storage", this.getState()); // eslint-disable-line
+        localStorage.state = JSON.stringify(this.getState());
+      }
     },
     getState() {
       return { ...state };
     }
   },
   created() {
+    if (localStorage.state) {
+      this.loadState(JSON.parse(localStorage.state));
+    }
     this.$on("CORRECT_ANSWER", function() {
       state.right_count += 1;
+      state.streak += 1;
       this.stateChange();
     });
     this.$on("INCORRECT_ANSWER", function() {
       state.wrong_count += 1;
+      state.streak = 0;
       this.stateChange();
     });
   }
